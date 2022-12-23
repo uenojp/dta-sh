c085954bda8a9bbd05b26178712fc3baa3b804a8
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-12-23 18:23:40 +0900

    Add setup scripts to install libdft64 and Intel Pin

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## 動的テイント解析を用いた悪性シェルスクリプトによる情報漏洩の検知
+
+### Install
+```sh
+# Intel Pinとlibdft64のインストール
+./setup.sh
+# 環境変数などの設定
+source ./env.init
+
+# libdf64の動作確認
+cd libdft64/tools && make test_mini
+```
+
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+
+# Install dependencies.
+sudo apt install \
+    build-essential \
+    curl \
+    g++-multilib \
+    gcc-multilib \
+    python3 \
+    wget \
+    ;
+
+
+# Extract Intel Pin.
+PIN=pin-3.20-98437-gf02b61307-gcc-linux
+if [ ! -d "$PIN" ]; then
+    tar xvf "$PIN.tar.gz"
+fi
+
+# Install & Build libdft64.
+# > PIN_ROOT: Specify the location for the Pin kit when building a tool outside of the kit.
+# ref. https://software.intel.com/sites/landingpage/pintool/docs/98650/Pin/doc/html/index.html#UsefulVariables
+export PIN_ROOT="$PWD/$PIN"
+if [ ! -d libdft64 ]; then
+    git clone https://github.com/AngoraFuzzer/libdft64
+fi
+cd libdft64
+make -j"$(nproc)"
+
+echo -e "\n\nRun \033[1msource env.init\033[0m\n"
+
