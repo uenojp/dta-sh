f398dc436bac368fd7607f2da2f224d47a53e75f
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-12-28 15:40:19 +0900

    Move `echo 0 > ptrace_scope` from env.init to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 set -eu
 
@@ -12,7 +12,6 @@ sudo apt install \
     wget \
     ;
 
-
 # Extract Intel Pin.
 PIN=pin-3.20-98437-gf02b61307-gcc-linux
 if [ ! -d "$PIN" ]; then
@@ -21,6 +20,7 @@ fi
 
 # Install & Build libdft64.
 # > PIN_ROOT: Specify the location for the Pin kit when building a tool outside of the kit.
+# (PIN_ROOT is for libdft64 builds)
 # ref. https://software.intel.com/sites/landingpage/pintool/docs/98650/Pin/doc/html/index.html#UsefulVariables
 export PIN_ROOT="$PWD/$PIN"
 if [ ! -d libdft64 ]; then
@@ -29,4 +29,9 @@ fi
 cd libdft64
 make -j"$(nproc)" || true
 
+# Set ptrace_scope as 0 for Intel Pin.
+# ref. https://www.kernel.org/doc/Documentation/security/Yama.txt
+echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope > /dev/null
+
 echo -e "\n\nRun \033[1msource env.init\033[0m\n"
+
