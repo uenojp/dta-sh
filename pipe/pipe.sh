a8f67fb9c3ac97a99e65fbfdad5f2d16bbb80a1b
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-01-24 21:44:44 +0900

    tmp

---
+++ pipe/pipe.sh
@@ -0,0 +1,2 @@
+# cat /etc/passwd | curl -X POST -d @- localhost:9999
+cat /etc/services | curl -X POST -d @- localhost:9999
--- src/sendfile.sh
+++ src/sendfile.sh
@@ -1,5 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-# cat /etc/passwd | curl -X POST -d @- localhost:9999
-seq 100 | curl -X POST -d @- localhost:9999
-# cat /etc/passwd | nl
+cat /etc/passwd | curl -X POST -d @- localhost:9999
+
+# curl -X POST -d "$(cat /etc/passwd)" localhost:9999
+
+# data="$(cat /etc/passwd)"
+# curl -X POST -d "$data" localhost:9999
+
+# seq 100 | curl -X POST -d @- localhost:9999
